894fdcc6d4dd719dea338da97b32aef163dfa69b
leonardvsc <leonardoworld@outlook.com>
2023-07-29 10:35:32 -0300

    Commit

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar.png")
   }
 }
+
+
+const eventos = document.querySelectorAll('.evento');
+    let currentIndex = 0;
+
+    function showEvent(index) {
+        eventos.forEach((evento, i) => {
+            evento.style.display = i === index ? 'block' : 'none';
+        });
+    }
+
+    function nextEvent() {
+        currentIndex = (currentIndex + 1) % eventos.length;
+        showEvent(currentIndex);
+    }
+
+    function prevEvent() {
+        currentIndex = (currentIndex - 1 + eventos.length) % eventos.length;
+        showEvent(currentIndex);
+    }
+
+    showEvent(currentIndex);
+
+    document.querySelector('.btn-next').addEventListener('click', nextEvent);
+    document.querySelector('.btn-prev').addEventListener('click', prevEvent);
